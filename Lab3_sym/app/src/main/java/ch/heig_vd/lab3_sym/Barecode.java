ecf39b32be78013dd4228914b97a6344fbb2e7e7
simon-baehler <simon.baehler@heig-vd.ch>
2016-12-08 15:20:14 +0100

    suite barcode

--- Lab3_sym/app/src/main/java/ch/heig_vd/lab3_sym/Barecode.java
+++ Lab3_sym/app/src/main/java/ch/heig_vd/lab3_sym/Barecode.java
@@ -3,8 +3,11 @@ package ch.heig_vd.lab3_sym;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Barecode extends AppCompatActivity {
+
+    private TextView txt_scan = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ public class Barecode extends AppCompatActivity {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
             // handle scan result
+            txt_scan = (TextView)findViewById(R.id.txt_scan);
+            txt_scan.setText(scanResult.getContents());
         }
         // else continue with any other code you need in the method
     }
