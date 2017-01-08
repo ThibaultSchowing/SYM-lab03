b2a877d291fdfb4ee82a2a3a0369a5f47a54c687
Amine Tayaa <amine.tayaa@gmail.com>
2017-01-08 13:22:56 +0100

    Ibeacon

--- Lab3_sym/app/src/main/java/ch/heig_vd/lab3_sym/Ibeacon.java
+++ Lab3_sym/app/src/main/java/ch/heig_vd/lab3_sym/Ibeacon.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -25,15 +26,19 @@ import java.util.Collection;
 public class Ibeacon extends AppCompatActivity implements BeaconConsumer {
     protected static final String TAG = "IbeaconActivity";
 
-    private static final String UUID = "XXXXXXXXXXXXXXXXXXXX";
+   // private static final String UUID = "XXXXXXXXXXXXXXXXXXXX";
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
     private BeaconManager beaconManager;
+
+    TextView IBeaconTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibeacon);
+
+        IBeaconTxt = (TextView) findViewById(R.id.IBeaconTxt);
 
         if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.M) {
 
@@ -55,6 +60,8 @@ public class Ibeacon extends AppCompatActivity implements BeaconConsumer {
             }
         }
         beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.setForegroundBetweenScanPeriod(1);
+        beaconManager.setForegroundScanPeriod(500);
         beaconManager.bind(this);
     }
 
@@ -66,22 +73,31 @@ public class Ibeacon extends AppCompatActivity implements BeaconConsumer {
 
     @Override
     public void onBeaconServiceConnect() {
-        beaconManager.setRangeNotifier(new RangeNotifier() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                Log.i(TAG, "range, size" + beacons.size());
                 if (beacons.size() > 0) {
                     Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away. Id 1: " + beacons.iterator().next().getId1()
                                 +", Id 2: " + beacons.iterator().next().getId2() +" and Id 3: "+ beacons.iterator().next().getId3());
+
+                   setText(IBeaconTxt, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away. Id 1: " + beacons.iterator().next().getId1()
+                            +", Id 2: " + beacons.iterator().next().getId2() +" and Id 3: "+ beacons.iterator().next().getId3());
+                } else {
+                   setText(IBeaconTxt, "No beacon found");
                 }
             }
         });
 
-        try {
-            beaconManager.startRangingBeaconsInRegion(new Region("myUniqueId", null, null, null));
-        } catch (RemoteException e) {   }
+        Log.i(TAG, "connecter");
 
         beaconManager.getBeaconParsers().add(new BeaconParser()
                 .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myUniqueId", null, null, null));
+        } catch (RemoteException e) {  e.printStackTrace(); }
+
+
 
     }
 
@@ -113,5 +129,13 @@ public class Ibeacon extends AppCompatActivity implements BeaconConsumer {
                 return;
             }
         }
+    }
+    private void setText(final TextView text,final String value){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(value);
+            }
+        });
     }
 }
